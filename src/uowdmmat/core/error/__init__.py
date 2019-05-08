be9e964bbb1d27706e9b8aaed699eaa762e63879
csterling <coreytsterling@gmail.com>
2019-05-08 13:29:25 +1200

    Renamed error modules to disambiguate class names from module names.

--- src/uowdmmat/core/error/__init__.py
+++ src/uowdmmat/core/error/__init__.py
@@ -1,5 +1,5 @@
 # Aliases
-from uowdmmat.core.error.MatrixAlgorithmsError import MatrixAlgorithmsError
-from uowdmmat.core.error.InvalidAxisError import InvalidAxisError
-from uowdmmat.core.error.InvalidShapeError import InvalidShapeError
-from uowdmmat.core.error.MatrixInversionError import MatrixInversionError
+from uowdmmat.core.error._MatrixAlgorithmsError import MatrixAlgorithmsError
+from uowdmmat.core.error._InvalidAxisError import InvalidAxisError
+from uowdmmat.core.error._InvalidShapeError import InvalidShapeError
+from uowdmmat.core.error._MatrixInversionError import MatrixInversionError
