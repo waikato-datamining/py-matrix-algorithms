588cc7da8f4366ffbd9630852283e29d37a96cd7
csterling <coreytsterling@gmail.com>
2019-05-14 13:18:56 +1200

    Made LoggingObject abstract.

--- src/uowdmmat/core/_LoggingObject.py
+++ src/uowdmmat/core/_LoggingObject.py
@@ -13,10 +13,12 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from abc import ABC
+
 from .java import Logger
 
 
-class LoggingObject:
+class LoggingObject(ABC):
     """
     Ancestor of objects with logging support.
     """
