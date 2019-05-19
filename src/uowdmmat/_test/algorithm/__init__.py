219c0fa029a7d2948a2184b7cbe431d493bc3792
csterling <coreytsterling@gmail.com>
2019-05-20 11:13:28 +1200

    Implementation of PCA algorithm and tests for such.

--- src/uowdmmat/_test/algorithm/__init__.py
+++ src/uowdmmat/_test/algorithm/__init__.py
@@ -14,5 +14,4 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from ._AbstractAlgorithm import AbstractAlgorithm
-from ._PCA import PCA
+from ._PCATest import PCATest
